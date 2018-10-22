17736931b56ea70b999fc05f9c7c9e7e1a3443cd
alvintangzc <4008321@qq.com>
2018-10-22 16:56:19 +0800

    add split

--- main.cpp
+++ main.cpp
@@ -1,39 +1,28 @@
 #include "stdio.h"
-#include "rapidjson/document.h"
-#include "rapidjson/writer.h"
-#include "rapidjson/stringbuffer.h"
-#include "rapidjson/error/en.h"
 #include <iostream>
 
 #include "easylogging++.h"
 
 #include "boost/format.hpp"
+#include "boost/lexical_cast.hpp"
+#include "boost/algorithm/string.hpp"
 
-using namespace rapidjson;
-
+using namespace std;
 INITIALIZE_EASYLOGGINGPP
 
 int main() {
     LOG(INFO) << "Hello, world";
-    std::cout << boost::format("%s %s") % "Hello" % 3 << std::endl;
-    // 1. 把 JSON 解析至 DOM。
-    const char* json = "{\"project\\t123\":\"rapidjson\",\"stars\":10}";
-    Document d;
+    LOG(INFO) << boost::format("%s %s") % "Hello" % 3 << std::endl;
+    LOG(INFO) << boost::lexical_cast<std::string>(123);
+    LOG(INFO) << boost::lexical_cast<int>("123");
+    LOG(INFO) << boost::lexical_cast<int>("05");
+    string stringtobesplit = "AA/BB-CC";
+    vector<string> tokens;
+    boost::split(tokens, stringtobesplit, boost::is_any_of("/-")); 
 
-    if (d.Parse(json).HasParseError())
+    for (auto &item : tokens)
     {
-        printf("%s\r\n",rapidjson::GetParseError_En(d.GetParseError()));
-        return 1;
+        LOG(INFO) << item;
     }
-    // 2. 利用 DOM 作出修改。
-    Value& s = d["stars"];
-    s.SetInt(s.GetInt() + 1);
-    // 3. 把 DOM 转换（stringify）成 JSON。
-    StringBuffer buffer;
-    Writer<StringBuffer> writer(buffer);
-    d.Accept(writer);
-    // Output {"project":"rapidjson","stars":11}
-    std::cout << buffer.GetString() << std::endl;
-    printf("%s\r\n", buffer.GetString());
-    return 0;
+   return 0;
 }
