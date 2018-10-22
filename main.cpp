4642c82acc8ad6a9fa86fab98607da98dd7ef0d0
alvintangzc <4008321@qq.com>
2018-10-22 20:58:40 +0800

    change to spdlog

--- main.cpp
+++ main.cpp
@@ -1,28 +1,29 @@
 #include "stdio.h"
 #include <iostream>
 
-#include "easylogging++.h"
+#include "spdlog/spdlog.h"
+#include "spdlog/sinks/stdout_color_sinks.h"
 
 #include "boost/format.hpp"
 #include "boost/lexical_cast.hpp"
 #include "boost/algorithm/string.hpp"
 
 using namespace std;
-INITIALIZE_EASYLOGGINGPP
+auto console = spdlog::stdout_color_mt("console");
 
 int main() {
-    LOG(INFO) << "Hello, world";
-    LOG(INFO) << boost::format("%s %s") % "Hello" % 3 << std::endl;
-    LOG(INFO) << boost::lexical_cast<std::string>(123);
-    LOG(INFO) << boost::lexical_cast<int>("123");
-    LOG(INFO) << boost::lexical_cast<int>("05");
+    console->info("Hello, world");
+    console->info("{}", (boost::format("%s %s") % "Hello" % 3).str());
+    console->info("{}", boost::lexical_cast<string>(123));
+    console->info("{}", boost::lexical_cast<int>("123"));
+    console->info("{}", boost::lexical_cast<int>("05"));
     string stringtobesplit = "AA/BB-CC";
     vector<string> tokens;
     boost::split(tokens, stringtobesplit, boost::is_any_of("/-")); 
 
     for (auto &item : tokens)
     {
-        LOG(INFO) << item;
+        console->info("{}", item);
     }
    return 0;
 }
